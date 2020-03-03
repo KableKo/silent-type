377f9f2b93bbbe33be42c79dccb43eefb7c8c2ff
Trent Kable <trent@kablekompany.com>
2020-03-03 06:23:16 -0600

    removed (id). Voids as it should now and hides type notis

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+/* eslint-disable no-empty-function */
 const { Plugin } = require('powercord/entities');
 const { typing } = require('powercord/webpack');
 
 module.exports = class SilentTyping extends Plugin {
-    startPlugin() {
-        this.oldStartTyping = typing.startTyping;
-        typing.startTyping = (id) => { };
-    }
-    pluginWillUnload() {
-        typing.startTyping = this.oldStartTyping;
-    }
+  startPlugin () {
+    this.oldStartTyping = typing.startTyping;
+    typing.startTyping = () => { };
+  }
+
+  pluginWillUnload () {
+    typing.startTyping = this.oldStartTyping;
+  }
 };
