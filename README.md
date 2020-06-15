c6e5b1b458d52a01e8a955ca6f8fe2da01778444
kablekompany <trent@kablekompany.com>
2020-06-14 22:30:33 -0500

    init

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# powercord-silenttyping
+# Silent Type
 
-A small Powercord plugin to disable your typing status.
+A small Powercord plugin to disable your typing status, or re-enable it with the use of [p]qtype [--on|--off].
+
+[Powercord](https://powercord.dev) is a Discord client that's snappy and lightweight, and offers a range of tools and tricks that can be used to interact with the platform.
+
+### Thanks Aetheryx for the help, always.
--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 const { Plugin } = require('powercord/entities');
 const { typing } = require('powercord/webpack');
 
-module.exports = class SilentTyping extends Plugin {
-  startPlugin () {
-    this.oldStartTyping = typing.startTyping;
-    typing.startTyping = () => { };
-  }
+module.exports = class SilentType extends Plugin {
+    startPlugin() {
+        this.oldStartTyping = typing.startTyping;
+        typing.startTyping = () => {};
+    }
 
-  pluginWillUnload () {
-    typing.startTyping = this.oldStartTyping;
-  }
+    pluginWillUnload() {
+        typing.startTyping = this.oldStartTyping;
+    }
 };
+/* TO-DO Command switch capbility */
