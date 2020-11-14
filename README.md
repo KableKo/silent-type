3f1682da092e5c8972a5bebba61ee2b709c0126a
Corban Amouzou <Corban@Amouzous.local>
2020-11-14 13:28:12 -0800

    Completed Todo

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A small Powercord plugin to disable your typing status.
 [Powercord](https://powercord.dev) is a Discord client that's snappy and lightweight, and offers a range of tools and tricks that can be used to interact with the platform.
 
 ## To-Do
-- [ ] Add switch capability to show/hide type notifier (`the use of [p]qtype [--on|--off].`)
+- [x] Add switch capability to show/hide type notifier (`the use of [p]qtype [--on|--off].`)
 
 ### Thanks Aetheryx for the help, always
--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 /* eslint-disable no-empty-function */
 const { Plugin } = require('powercord/entities');
-const { typing } = require('powercord/webpack');
-
 module.exports = class SilentType extends Plugin {
     startPlugin() {
-        this.oldStartTyping = typing.startTyping;
-        typing.startTyping = () => {};
+        powercord.api.commands.registerCommand({
+            command: 'silent-type',
+            description: 'Keep your typing to yourself!',
+            usage: '{c} [--on|--off]',
+            executor: (args) => ({
+                send: false,
+                result:  this.handler(args)
+            })
+        })
+        // Magic
+        this.callRefresh()
+    }
+     handler(args) {
+        args = args.map(a => a.toLowerCase())
+        if (args.includes('--off') && args.includes('--on')) {
+            return "Oops! Instead of receiving one flag I got both!\nSend only one flag next time"
+        } else if (args.includes('--on')) {
+            if (this.settings.get('on', true)) {
+                return "I am already turned on\nIf you would like to turn me off use the `--off` flag"
+            }
+            this.settings.set('on')
+            this.callRefresh()
+            return "Turned On"
+        } else if (args.includes('--off')) {
+            if (!this.settings.get('on')) {
+                return "I am already turned off\nIf you would like to turn me on use the `--on` flag"
+            }
+            this.settings.set('on')
+            this.callRefresh()
+            return "Turned Off"
+        } else {
+            return `Incorrect Flags or no flags given\nPlease run \`${powercord.api.commands.prefix}help silent-type\` for command usage`
+        }
+    }
+
+    callRefresh() {
+        try {
+            const { typing } = require('powercord/webpack');
+            typing.startTyping = (startTyping => (channel) => setImmediate(() => {
+                const on = this.settings.get('on')
+                if (!on) {
+                    startTyping(channel)
+                }
+            }))(this.oldStartTyping = typing.startTyping)
+        } catch (e) {
+        }
     }
 
     pluginWillUnload() {
